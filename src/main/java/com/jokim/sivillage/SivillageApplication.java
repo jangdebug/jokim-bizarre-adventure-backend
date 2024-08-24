3790fbb3f6ed612c57353f8ce987b885777dbe56
Gyuchan Kim <sb3g@naver.com>
2024-08-24 23:53:18 +0900

    🎉 init: Create initial project structure

---
+++ src/main/java/com/jokim/sivillage/SivillageApplication.java
@@ -0,0 +1,13 @@
+package com.jokim.sivillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SivillageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SivillageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jokim/sivillage/SivillageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jokim.sivillage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SivillageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
