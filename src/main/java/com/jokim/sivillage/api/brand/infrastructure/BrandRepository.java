1830b1b755aee8f3783440525d408da7f3fe2dd0
kimdayday <kdh39278@gmail.com>
2024-09-24 19:24:34 +0900

    :recycle: fix: fix ProductCategoryListRepoList for returning 20 items

--- src/main/java/com/jokim/sivillage/api/brand/infrastructure/BrandRepository.java
+++ src/main/java/com/jokim/sivillage/api/brand/infrastructure/BrandRepository.java
@@ -1,13 +1,12 @@
 package com.jokim.sivillage.api.brand.infrastructure;
 
 import com.jokim.sivillage.api.brand.domain.Brand;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BrandRepository extends JpaRepository<Brand, Long> {
 
 
-    Optional<Brand> findByMainName(String mainName);
+//    Optional<Brand> findByMainName(String mainName);
 
-    Optional<Brand> findByBrandCode(String brandCode);
+//    Optional<Brand> findByBrandCode(String brandCode);
 }
