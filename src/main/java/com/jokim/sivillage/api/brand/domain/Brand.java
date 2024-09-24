40accbc9da1f4b86dabe5e9007f8a3c00acde9e5
kimdayday <kdh39278@gmail.com>
2024-09-24 16:26:19 +0900

    :recycle: fix: merge brand file from develop

--- src/main/java/com/jokim/sivillage/api/brand/domain/Brand.java
+++ src/main/java/com/jokim/sivillage/api/brand/domain/Brand.java
@@ -1,22 +1,35 @@
 package com.jokim.sivillage.api.brand.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.ToString;
+import lombok.NoArgsConstructor;
 
-@Entity
+@Builder
 @Getter
-@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Brand {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false, length = 36)
     private String brandCode;
+
     @Column(length = 30)
-    private String mainName;
+    private String englishName;
+
     @Column(length = 30)
-    private String subName;
+    private String koreanName;
+
+    @Column(nullable = false, length = 10)
+    private String englishInitial;
+
+    @Column(nullable = false, length = 10)
+    private String koreanInitial;
 
 }
